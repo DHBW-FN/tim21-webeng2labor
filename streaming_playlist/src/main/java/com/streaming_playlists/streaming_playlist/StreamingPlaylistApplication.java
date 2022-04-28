18c7fe880a38d60b5805bbfc1bd41fcce32be045
Niklas Hornung <niklas2002hornung@t-online.de>
2022-04-28 14:00:18 +0200

    init project

---
+++ streaming_playlist/src/main/java/com/streaming_playlists/streaming_playlist/StreamingPlaylistApplication.java
@@ -0,0 +1,13 @@
+package com.streaming_playlists.streaming_playlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingPlaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingPlaylistApplication.class, args);
+	}
+
+}
---
+++ streaming_playlist/src/test/java/com/streaming_playlists/streaming_playlist/StreamingPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming_playlists.streaming_playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingPlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
