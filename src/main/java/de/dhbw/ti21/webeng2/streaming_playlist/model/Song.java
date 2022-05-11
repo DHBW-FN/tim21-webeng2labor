634af90c5665c6fdd14cddc124f775b13c11b6f1
Tobias Goetz <tobigoetz98@gmail.com>
2022-05-11 19:02:24 +0200

    Added song data class

---
+++ src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/model/Song.java
@@ -0,0 +1,31 @@
+package de.dhbw.ti21.webeng2.streaming_playlist.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "songs")
+public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "releaseDate")
+    private int releaseDate;
+}
