bb2c669e87254b085339b477fb82e6d6667bfc24
Tobias Goetz <tobigoetz98@gmail.com>
2022-05-11 18:51:42 +0200

    Added swagger and h2 db

--- src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/StreamingPlaylistApplication.java
+++ src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/StreamingPlaylistApplication.java
@@ -1,9 +1,11 @@
-package de.dbbw.ti21.webeng2.streaming_playlist;
+package de.dhbw.ti21.webeng2.streaming_playlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class StreamingPlaylistApplication {
 
 	public static void main(String[] args) {
