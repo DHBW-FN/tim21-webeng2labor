8d9e7904c509cd454182461c21b1b2a57423bfaa
Tobias Goetz <tobigoetz98@gmail.com>
2022-05-11 19:17:13 +0200

    Added artist data class

---
+++ src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/model/Artist.java
@@ -0,0 +1,28 @@
+package de.dhbw.ti21.webeng2.streaming_playlist.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "artists")
+public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+}
