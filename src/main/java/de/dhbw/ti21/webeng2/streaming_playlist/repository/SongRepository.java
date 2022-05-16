8cd4914307e57b8bf180274522e4a736045e18f1
Tobias Goetz <tobigoetz98@gmail.com>
2022-05-16 10:20:27 +0200

    Feature/i30 create song repository (#33)
    * Added song repository

---
+++ src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/repository/SongRepository.java
@@ -0,0 +1,13 @@
+package de.dhbw.ti21.webeng2.streaming_playlist.repository;
+
+import de.dhbw.ti21.webeng2.streaming_playlist.model.Artist;
+import de.dhbw.ti21.webeng2.streaming_playlist.model.Song;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SongRepository extends JpaRepository<Song, Long> {
+
+    List<Song> findById(long id);
+    List<Song> findByTitle(String title);
+}
