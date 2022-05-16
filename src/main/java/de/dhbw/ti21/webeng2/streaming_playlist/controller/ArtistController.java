68ab9dd28097fff82d45b9791bf2615978c8f454
Niklas Hornung <niklas2002hornung@t-online.de>
2022-05-16 20:00:46 +0200

    added postArtist

---
+++ src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/controller/ArtistController.java
@@ -0,0 +1,28 @@
+package de.dhbw.ti21.webeng2.streaming_playlist.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.dhbw.ti21.webeng2.streaming_playlist.model.Artist;
+import de.dhbw.ti21.webeng2.streaming_playlist.repository.ArtistRepository;
+import io.swagger.models.Response;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+public class ArtistController {
+    private ArtistRepository repository;
+
+    @PostMapping("/artist")
+    public ResponseEntity<Artist> postArtist(@RequestBody Artist artist){
+        try{
+            return new ResponseEntity<Artist>(this.repository.save(artist), HttpStatus.OK);
+        }
+        catch (Exception ex){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/repository/ArtistRepository.java
+++ src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/repository/ArtistRepository.java
@@ -3,7 +3,7 @@ package de.dhbw.ti21.webeng2.streaming_playlist.repository;
 import de.dhbw.ti21.webeng2.streaming_playlist.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArtistInterface extends JpaRepository<Artist, Long> {
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
     Artist findById(long id);
     Artist findByName(String name);
