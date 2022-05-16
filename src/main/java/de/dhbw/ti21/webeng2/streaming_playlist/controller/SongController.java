0f8b40fd6d9957700e641a8f2621a578913724ea
Tobias Goetz <tobigoetz98@gmail.com>
2022-05-16 10:51:58 +0200

    Added Get endpoint for song

---
+++ src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/controller/SongController.java
@@ -0,0 +1,35 @@
+package de.dhbw.ti21.webeng2.streaming_playlist.controller;
+
+import de.dhbw.ti21.webeng2.streaming_playlist.model.Song;
+import de.dhbw.ti21.webeng2.streaming_playlist.repository.SongRepository;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+public class SongController {
+
+    private SongRepository repository;
+
+    @GetMapping( "/song")
+    public ResponseEntity<List<Song>> getSong(@RequestParam(required = false) int id){
+        try{
+            List<Song> songs = new ArrayList<>();
+
+            if(id == 0){
+                return new ResponseEntity<>(this.repository.findAll(), HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(this.repository.findById(id), HttpStatus.OK);
+        }
+        catch (Exception ex){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
