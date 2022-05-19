f6d695ff52e1889be8185feee1c45059fb18060a
Niklas Hornung <niklas2002hornung@t-online.de>
2022-05-19 13:12:04 +0200

    added getter and setter

--- src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/model/Artist.java
+++ src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/model/Artist.java
@@ -33,4 +33,47 @@ public class Artist {
         this.description = description;
         
     }
+
+    /**
+     * @return long return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
