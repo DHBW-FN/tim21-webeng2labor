d8727802213dc0954fbabbbd213d9f4cb8b2fa75
Tobias Goetz <tobigoetz98@gmail.com>
2022-05-23 16:13:08 +0200

    Added post endpoint to create playlists (#47)

--- src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/controller/PlaylistController.java
+++ src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/controller/PlaylistController.java
@@ -1,21 +1,22 @@
 package de.dhbw.ti21.webeng2.streaming_playlist.controller;
 
 import de.dhbw.ti21.webeng2.streaming_playlist.model.Playlist;
+import de.dhbw.ti21.webeng2.streaming_playlist.model.Song;
 import de.dhbw.ti21.webeng2.streaming_playlist.repository.PlaylistRepository;
+import de.dhbw.ti21.webeng2.streaming_playlist.repository.SongRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/playlist")
 public class PlaylistController {
     private PlaylistRepository playlistRepository;
+    private SongRepository songRepository;
 
     @GetMapping
     public ResponseEntity<List<Playlist>> getPlaylist(@RequestParam(required = false) Map<String, String> params){
@@ -48,6 +49,39 @@ public class PlaylistController {
         }
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Playlist> postPlaylist(@RequestBody Playlist playlist){
+        try{
+            //remove duplicate songs from song
+            playlist.setSongs(new HashSet<>(playlist.getSongs()));
+
+            Set<Song> songs = new HashSet<>(playlist.getSongs());
+            for (Song song : playlist.getSongs()) {
+                songs.remove(song);
+
+                if (this.songRepository.findById(song.getId()) != null) {
+                    songs.add(this.songRepository.findById(song.getId()));
+                    continue;
+                }
+
+                if(this.songRepository.findFirstByTitleContaining(song.getTitle()) != null) {
+                    songs.add(this.songRepository.findFirstByTitleContaining(song.getTitle()));
+                    continue;
+                }
+
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+            playlist.setSongs(songs);
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(this.playlistRepository.save(playlist));
+        }
+        catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @DeleteMapping
