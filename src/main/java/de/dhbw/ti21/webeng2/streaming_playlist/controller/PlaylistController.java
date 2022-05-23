d2f9665cf4a8be5d77977cfe76e4c20b9cbed875
Tobias Goetz <tobigoetz98@gmail.com>
2022-05-23 16:02:32 +0200

    Added delete mapping for playlist

--- src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/controller/PlaylistController.java
+++ src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/controller/PlaylistController.java
@@ -5,10 +5,7 @@ import de.dhbw.ti21.webeng2.streaming_playlist.repository.PlaylistRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -51,5 +48,20 @@ public class PlaylistController {
         }
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletePlaylist(@RequestParam Long id){
+        try{
+            if(!this.playlistRepository.existsById(id)){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+
+            this.playlistRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+        catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
