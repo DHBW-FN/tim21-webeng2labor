ed1ee67194df72b66c6ef229b5163c0c12d944e2
TobiasGoetz <tobigoetz98@gmail.com>
2022-05-28 00:36:08 +0200

    Removed every class from swagger that is not annotated with @RestController

--- src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/SpringFoxConfig.java
+++ src/main/java/de/dhbw/ti21/webeng2/streaming_playlist/SpringFoxConfig.java
@@ -2,6 +2,7 @@ package de.dhbw.ti21.webeng2.streaming_playlist;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -16,7 +17,7 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
